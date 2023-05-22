d0424bfa43dd25e14ed8198e0cc04b147feb62fd
r4z4 <r4z4aa@gmail.com>
2023-05-22 22:29:52 +0000

    workflow and update test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,14 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Projects link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Projects/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders Articles link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Articles/i);
   expect(linkElement).toBeInTheDocument();
 });
