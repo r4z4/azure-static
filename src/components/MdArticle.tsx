be41c11350024e8b1caf874e858080f4d9cb0927
r4z4 <r4z4aa@gmail.com>
2023-06-05 02:23:47 +0000

    little clean

--- src/components/MdArticle.tsx
+++ src/components/MdArticle.tsx
@@ -7,28 +7,41 @@ interface MdArticleProps {
     title: string;
     subDir: string;
 }
+const run3html = 
+  "<div><style scoped>.dataframe tbody tr th:only-of-type{vertical-align:middle}.dataframe tbody tr th{vertical-align:top}.dataframe thead th{text-align:right}</style><table border='1' class='dataframe'><thead><tr style='text-align:right'><th></th><th>newsgroup</th><th>subject</th></tr></thead><tbody><tr><th>0</th><td>autos</td><td>saturn's pricing policy</td></tr><tr><th>10</th><td>autos</td><td>are bmw's worth the price</td></tr><tr><th>12</th><td>autos</td><td>re headlights problem</td></tr><tr><th>14</th><td>autos</td><td>left turn signal won't stop automaticaly</td></tr><tr><th>16</th><td>autos</td><td>what is volvo</td></tr></tbody></table></div>"
+const run4html = 
+  "<div><style scoped>.dataframe tbody tr th:only-of-type{vertical-align:middle}.dataframe tbody tr th{vertical-align:top}.dataframe thead th{text-align:right}</style><table border='1' class='dataframe'><thead><tr style='text-align:right'><th></th><th>newsgroup</th><th>subject</th></tr></thead><tbody><tr><th>0</th><td>autos</td><td>saturn's pricing policy</td></tr><tr><th>10</th><td>autos</td><td>are bmw's worth the price</td></tr><tr><th>12</th><td>autos</td><td>re headlights problem</td></tr><tr><th>14</th><td>autos</td><td>left turn signal won't stop automaticaly</td></tr><tr><th>16</th><td>autos</td><td>what is volvo</td></tr></tbody></table></div>"
 
 function MdArticle({ title, subDir }: MdArticleProps) {
     const mdPath = require(`../articles/${subDir}/${title}.md`)
     const [terms, setTerms] = React.useState('')
+    const [html, setHtml] = React.useState('')
 
     const imagePaths = [`../assets/article_images/${subDir}/${title}.png`]
 
     React.useEffect(() => {
+        if (subDir === 'glove') { 
+          if (title === 'run_03') {
+            setHtml(run3html) 
+          }
+          if (title === 'run_04') {
+            setHtml(run4html) 
+          }
+        }
         fetch(mdPath).then((response) => response.text()).then((text) => {
             
             setTerms(text)
         })
         // setTerms(markdownTable)
-    }, [mdPath])
+    }, [mdPath, subDir, title])
 
     return (
-      <div className="grid-container">
+      <div className='grid-container'>
         <div>
           <ReactMarkdown className={style.reactMarkDown} children={terms} />
         </div>
         <div className='flex-container'>
-          <SidePanelImageDisplay imagePaths={imagePaths} />
+          <SidePanelImageDisplay html={html} imagePaths={imagePaths} />
         </div>
       </div>
     )
